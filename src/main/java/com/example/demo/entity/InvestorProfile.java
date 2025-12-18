68b018091104de472851f904f2922ce551c4fcd9
bala-amypo <bala@amypo.in>
2025-12-18 17:35:53 +0000

    User commit

--- src/main/java/com/example/demo/entity/InvestorProfile.java
+++ src/main/java/com/example/demo/entity/InvestorProfile.java
@@ -27,5 +27,16 @@ public class InvestorProfile{
     public void setFullName(String fullName){this.fullName=fullName;}
 
     public String getEmail(){return email;}
-    public void setEmail(String email){this.email}
+    public void setEmail(String email){this.email=email;}
+
+    public boolean getActive(){return active;}
+    public void setActive(boolean active){this.active=active;}
+
+    public Date getDate(){return created;}
+    public void setDate(Date created){this.created=created;}
+
+    public InvestorProfile()
+    {
+
+    }
 }

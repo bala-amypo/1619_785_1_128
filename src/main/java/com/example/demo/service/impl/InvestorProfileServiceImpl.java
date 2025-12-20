f71a630e819af1f13fc3717d3c9a64d51fa174ca
bala-amypo <bala@amypo.in>
2025-12-20 19:05:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/InvestorProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/InvestorProfileServiceImpl.java
@@ -2,7 +2,7 @@ package com.example.demo.service.impl;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired
-import com.example.demo.service.Investor
+import com.example.demo.service.InvestorProfileService;
 
 @Service 
 public class InvestorProfileServiceImpl implements InvestorProfileService{

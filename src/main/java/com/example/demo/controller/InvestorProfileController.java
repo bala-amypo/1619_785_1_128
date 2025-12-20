4b781af520a4cf6939e4c287faf18b3751014313
bala-amypo <bala@amypo.in>
2025-12-20 19:00:58 +0000

    User commit

--- src/main/java/com/example/demo/controller/InvestorProfileController.java
+++ src/main/java/com/example/demo/controller/InvestorProfileController.java
@@ -2,7 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.service.InvestorProfileService;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController

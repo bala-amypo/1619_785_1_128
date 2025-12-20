779a8057f32ee877e62f56a86766c4ac32fd3d3b
bala-amypo <bala@amypo.in>
2025-12-20 10:51:36 +0000

    User commit

--- src/main/java/com/example/demo/repository/InvestorProfileRepository.java
+++ src/main/java/com/example/demo/repository/InvestorProfileRepository.java
@@ -1,6 +1,10 @@
 package com.example.demo.repository;
 
-
+import org.springframework.data.jpa.repository.JpsRepository;
+import org.springframework.sterotype.Repository;
+import com.example.demo.entity.
 
 @Repository
-public interface InvestorProfileRepository extends 
+public interface InvestorProfileRepository extends JpaRepository<InvestorProfile, Long>{
+
+}

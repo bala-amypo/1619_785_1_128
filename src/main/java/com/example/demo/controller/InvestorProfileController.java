e3ded5108f7b0fc3e79b282eea7900950e49a91b
bala-amypo <bala@amypo.in>
2025-12-20 13:49:37 +0000

    User commit

--- src/main/java/com/example/demo/controller/InvestorProfileController.java
+++ src/main/java/com/example/demo/controller/InvestorProfileController.java
@@ -6,5 +6,6 @@ package com.example.demo.controller;
 public class InvestorProfileController{
 
     @Autowired InvestorProfileService pservice;
-    @PostMapping("/")
+    @PostMapping("/createInvestordata")
+    public InvestorProfile maddata(@RequestBody )
 }

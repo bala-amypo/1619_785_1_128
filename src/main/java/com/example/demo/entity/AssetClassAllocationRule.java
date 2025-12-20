f384aae6b7891d777b5365b5d89d2a51f937335d
bala-amypo <bala@amypo.in>
2025-12-20 20:09:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/AssetClassAllocationRule.java
+++ src/main/java/com/example/demo/entity/AssetClassAllocationRule.java
@@ -1,9 +1,17 @@
 package com.example.demo.entity;
 
 
+import jakarta.persistence.*;
+import java.util.Date;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AssetClassAllocationRule{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long investorId;
     private Double targetPercentage;

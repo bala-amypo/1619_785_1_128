ca3bff803795b0af5b1f6660e07d79c1a9a08139
bala-amypo <bala@amypo.in>
2025-12-20 13:58:37 +0000

    User commit

--- src/main/java/com/example/demo/controller/InvestorProfileController.java
+++ src/main/java/com/example/demo/controller/InvestorProfileController.java
@@ -8,10 +8,12 @@ public class InvestorProfileController{
     @Autowired InvestorProfileService pservice;
     @PostMapping("/createInvestordata")
     public InvestorProfile maddata(@RequestBody InvestorProfile investor){
-        return pservice.createInvestor(investor)
+        return pservice.createInvestor(investor);
     }
     @GetMapping("/getInvestorByIds/{id}")
-    public List<InvestorProfile> dandata(){
-        return pservice.getInvestorById()
+    public List<InvestorProfile> dandata(@PathVariable Long id){
+        return pservice.getInvestorById(id);
     }
+    @GetMapping("")
+    
 }

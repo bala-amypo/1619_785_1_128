97db2435c8f079edc310805da2f82af8f691cf14
bala-amypo <bala@amypo.in>
2025-12-20 11:00:54 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/InvestorProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/InvestorProfileServiceImpl.java
@@ -1,16 +1,1 @@
 package com.example.demo.service.impl;
-
-
-
-@Service
-public class InvestorProfileServiceImpl implements InvestorProfileService{
-
-    @Autowired
-    private InvestorProfileServiceRepository repository;
-
-    @Override
-    public InvestorProfile createInvestor(InvestorProfile investor){
-        
-    }
-
-}

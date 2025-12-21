8885f15aebf724396348826eb1f94ff7c4ba50d4
bala-amypo <bala@amypo.in>
2025-12-21 13:09:02 +0000

    User commit

--- src/main/java/com/example/demo/service/InvestorProfileService.java
+++ src/main/java/com/example/demo/service/InvestorProfileService.java
@@ -1,14 +1,18 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.InvestorProfile;
+
 import java.util.List;
 
-public interface InvestorProfileService{
+public interface InvestorProfileService {
 
     InvestorProfile createInvestor(InvestorProfile investor);
-    InvestorProfile getInvestorById(Long id);
-    InvestorProfile findByInvestorId(String investorId);
-    List<InvestorProfile> getAllInvestor();
-    InvestorProfile updateInvestorStatus(Long id,boolean active);
 
+    InvestorProfile getInvestorById(Long id);
+
+    List<InvestorProfile> getAllInvestors();
+
+    InvestorProfile updateInvestorStatus(Long id, boolean active);
+
+    InvestorProfile findByInvestorId(String investorId);
 }

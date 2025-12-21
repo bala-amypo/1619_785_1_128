59d18ed97a2257bcf274b1e615c14216c733b6d4
bala-amypo <bala@amypo.in>
2025-12-21 12:39:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/InvestorProfile.java
+++ src/main/java/com/example/demo/entity/InvestorProfile.java
@@ -1,48 +1,42 @@
 package com.example.demo.entity;
-
-import jakarta.persistence.*;
-import java.util.Date;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 @Entity
+@Table(name = "investor_profiles")
 public class InvestorProfile {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(unique = true, nullable = false)
     private String investorId;
-    private String fullName;
-    private String email;
+    @Column(nullable = false)
+    private String name;
     private boolean active;
-    private Date created;
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getInvestorId() { return investorId; }
-    public void setInvestorId(String investorId) { this.investorId = investorId; }
-
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public boolean getActive() { return active; }
-    public void setActive(boolean active) { this.active = active; }
-
-    public Date getCreated() { return created; }
-    public void setCreated(Date created) { this.created = created; }
-
-    public InvestorProfile(Long id, String investorID, String fullName,
-                           String email, boolean active, Date created) {
-        this.id = id;
-        this.investorId = investorID;
-        this.fullName = fullName;
-        this.email = email;
-        this.active = active;
-        this.created = created;
+    public InvestorProfile() {
     }
-
-    public InvestorProfile() {}
+    public Long getId() {
+        return id;
+    }
+    public String getInvestorId() {
+        return investorId;
+    }
+    public void setInvestorId(String investorId) {
+        this.investorId = investorId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public boolean isActive() {
+        return active;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }

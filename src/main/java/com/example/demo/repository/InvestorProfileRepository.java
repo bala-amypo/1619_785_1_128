85fa059b827f9be3a3b6e5f73a7b91437d46a47d
bala-amypo <bala@amypo.in>
2025-12-26 09:14:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/InvestorProfileRepository.java
+++ src/main/java/com/example/demo/repository/InvestorProfileRepository.java
@@ -1,12 +1,12 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.InvestorProfile;
+import java.util.List;
+import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvestorProfileRepository extends JpaRepository<InvestorProfile, Long> {
-
-    InvestorProfile findByInvestorId(String investorId);
+public interface InvestorProfileRepository {
+    InvestorProfile save(InvestorProfile investor);
+    Optional<InvestorProfile> findById(Long id);
+    List<InvestorProfile> findAll();
+    Optional<InvestorProfile> findByInvestorId(String investorId);
 }

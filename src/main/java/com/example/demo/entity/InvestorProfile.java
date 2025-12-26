005f7faa1b6b76328e7e5c63f41f83b7a0fce8a8
bala-amypo <bala@amypo.in>
2025-12-26 09:43:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/InvestorProfile.java
+++ src/main/java/com/example/demo/entity/InvestorProfile.java
@@ -1,25 +1,28 @@
 package com.example.demo.entity;
 
 public class InvestorProfile {
+
     private Long id;
     private String investorId;
     private String fullName;
     private String email;
-    private boolean active;
+    private Boolean active;
 
-    public InvestorProfile(String investorId, String fullName, String email, boolean active) {
+    public InvestorProfile(String investorId, String fullName, String email, Boolean active) {
         this.investorId = investorId;
         this.fullName = fullName;
         this.email = email;
         this.active = active;
     }
 
-    // Getters and Setters
+    public InvestorProfile() {}
+
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
     public String getInvestorId() { return investorId; }
     public String getFullName() { return fullName; }
     public String getEmail() { return email; }
-    public boolean getActive() { return active; }
-    public void setActive(boolean active) { this.active = active; }
+    public Boolean getActive() { return active; }
+    public void setActive(Boolean active) { this.active = active; }
 }

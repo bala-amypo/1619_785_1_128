8d283ee4722e3cd0e559151ef076523054b20c4a
bala-amypo <bala@amypo.in>
2025-12-26 10:05:14 +0000

    User commit

--- src/main/java/com/example/demo/entity/AllocationSnapshotRecord.java
+++ src/main/java/com/example/demo/entity/AllocationSnapshotRecord.java
@@ -1,29 +1,17 @@
 package com.example.demo.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AllocationSnapshotRecord {
 
     private Long id;
-    private Long investorId;
-    private LocalDateTime snapshotDate;
-    private Double totalPortfolioValue;
-    private String detailsJson;
-
-    public AllocationSnapshotRecord(Long investorId, LocalDateTime snapshotDate,
-                                    Double totalPortfolioValue, String detailsJson) {
-        this.investorId = investorId;
-        this.snapshotDate = snapshotDate;
-        this.totalPortfolioValue = totalPortfolioValue;
-        this.detailsJson = detailsJson;
-    }
-
-    public AllocationSnapshotRecord() {}
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public Long getInvestorId() { return investorId; }
-    public LocalDateTime getSnapshotDate() { return snapshotDate; }
-    public Double getTotalPortfolioValue() { return totalPortfolioValue; }
+    private String investorId;
+    private String snapshotJson;
+    private LocalDateTime capturedAt;
 }
--- src/main/java/com/example/demo/entity/HoldingRecord.java
+++ src/main/java/com/example/demo/entity/HoldingRecord.java
@@ -1,30 +1,17 @@
 package com.example.demo.entity;
 
-import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import com.example.demo.entity.enums.AssetClassType;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class HoldingRecord {
 
     private Long id;
-    private Long investorId;
+    private String investorId;
     private AssetClassType assetClass;
-    private Double currentValue;
-    private LocalDateTime recordDate;
-
-    public HoldingRecord(Long investorId, AssetClassType assetClass,
-                         Double currentValue, LocalDateTime recordDate) {
-        this.investorId = investorId;
-        this.assetClass = assetClass;
-        this.currentValue = currentValue;
-        this.recordDate = recordDate;
-    }
-
-    public HoldingRecord() {}
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public Long getInvestorId() { return investorId; }
-    public AssetClassType getAssetClass() { return assetClass; }
-    public Double getCurrentValue() { return currentValue; }
+    private double currentValue;
 }

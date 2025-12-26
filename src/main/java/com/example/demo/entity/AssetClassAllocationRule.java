27d9e802111f064938cdbc031216a372842a5d03
bala-amypo <bala@amypo.in>
2025-12-26 10:04:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/AssetClassAllocationRule.java
+++ src/main/java/com/example/demo/entity/AssetClassAllocationRule.java
@@ -1,33 +1,17 @@
 package com.example.demo.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import com.example.demo.entity.enums.AssetClassType;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AssetClassAllocationRule {
 
     private Long id;
-    private Long investorId;
     private AssetClassType assetClass;
-    private Double targetPercentage;
-    private Boolean active;
-
-    public AssetClassAllocationRule(Long investorId, AssetClassType assetClass,
-                                    Double targetPercentage, Boolean active) {
-        this.investorId = investorId;
-        this.assetClass = assetClass;
-        this.targetPercentage = targetPercentage;
-        this.active = active;
-    }
-
-    public AssetClassAllocationRule() {}
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public Long getInvestorId() { return investorId; }
-    public AssetClassType getAssetClass() { return assetClass; }
-    public Double getTargetPercentage() { return targetPercentage; }
-    public void setTargetPercentage(Double targetPercentage) {
-        this.targetPercentage = targetPercentage;
-    }
-    public Boolean getActive() { return active; }
+    private double minPercentage;
+    private double maxPercentage;
 }

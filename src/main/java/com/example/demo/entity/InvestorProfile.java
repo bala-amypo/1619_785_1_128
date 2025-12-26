798a6f4966806f75f5c482d5d013abf2ca18cfc5
bala-amypo <bala@amypo.in>
2025-12-26 10:04:31 +0000

    User commit

--- src/main/java/com/example/demo/entity/InvestorProfile.java
+++ src/main/java/com/example/demo/entity/InvestorProfile.java
@@ -1,28 +1,16 @@
 package com.example.demo.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class InvestorProfile {
 
     private Long id;
     private String investorId;
-    private String fullName;
-    private String email;
-    private Boolean active;
-
-    public InvestorProfile(String investorId, String fullName, String email, Boolean active) {
-        this.investorId = investorId;
-        this.fullName = fullName;
-        this.email = email;
-        this.active = active;
-    }
-
-    public InvestorProfile() {}
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getInvestorId() { return investorId; }
-    public String getFullName() { return fullName; }
-    public String getEmail() { return email; }
-    public Boolean getActive() { return active; }
-    public void setActive(Boolean active) { this.active = active; }
+    private String name;
+    private boolean active;
 }

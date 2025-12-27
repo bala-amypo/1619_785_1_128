345ba38b6cac39f2fafe0bb0e9c4d3b5b6a6a770
bala-amypo <bala@amypo.in>
2025-12-27 04:22:47 +0000

    User commit

--- src/main/java/com/example/demo/entity/AllocationSnapshot.java
+++ src/main/java/com/example/demo/entity/AllocationSnapshot.java
@@ -8,7 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class AllocationSnapshotRecord {
+public class AllocationSnapshot {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

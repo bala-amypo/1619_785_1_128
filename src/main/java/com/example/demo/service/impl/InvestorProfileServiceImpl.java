f6b7ba6be48b83cc07a8f5a3fee53738e005c360
bala-amypo <bala@amypo.in>
2025-12-27 04:36:54 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/InvestorProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/InvestorProfileServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.InvestorProfile;
+import com.example.demo.service.InvestorProfileService;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.InvestorProfileRepository;
 import org.springframework.stereotype.Service;

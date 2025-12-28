97db56c0dfa794bc769f148b385fa83343c6790c
bala-amypo <bala@amypo.in>
2025-12-28 07:04:32 +0000

    User commit

--- src/main/java/com/example/demo/repository/HoldingRecordRepository.java
+++ src/main/java/com/example/demo/repository/HoldingRecordRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HoldingRepository extends JpaRepository<HoldingRecord, Long> {
-
+public interface HoldingRecordRepository
+        extends JpaRepository<HoldingRecord, Long> {
+    // DO NOT add findById()
 }

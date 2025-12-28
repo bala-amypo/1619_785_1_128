34bb05c99eb276650d5e8e5f9e81efa800742a61
bala-amypo <bala@amypo.in>
2025-12-28 08:33:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/HoldingRecordRepository.java
+++ src/main/java/com/example/demo/repository/HoldingRecordRepository.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.HoldingRecord;
 import com.example.demo.entity.enums.AssetClassType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository

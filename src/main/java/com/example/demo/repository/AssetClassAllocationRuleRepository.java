8b00f58e54ca2d62473ba66b2a50d095f83d45d9
bala-amypo <bala@amypo.in>
2025-12-29 10:13:41 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssetClassAllocationRuleRepository.java
+++ src/main/java/com/example/demo/repository/AssetClassAllocationRuleRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.AssetClassAllocationRule;
-// import com.example.demo.entity.enums.AssetClassType;
+
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
